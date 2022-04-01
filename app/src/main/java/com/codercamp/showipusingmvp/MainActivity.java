c13a57293a8a11fc4197967a8201ab729ed60884
shamimcse1 <mdshamimh529@gmail.com>
2022-04-01 18:16:59 +0600

    Added Network layer

--- app/src/main/java/com/codercamp/showipusingmvp/MainActivity.java
+++ app/src/main/java/com/codercamp/showipusingmvp/MainActivity.java
@@ -1,13 +1,64 @@
 package com.codercamp.showipusingmvp;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.codercamp.showipusingmvp.network.ApiInterface;
+import com.codercamp.showipusingmvp.network.RetrofitApiClient;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressDialog progressDialog;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Logger.addLogAdapter(new AndroidLogAdapter());
+        textView = findViewById(R.id.txtView);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Loading....");
+
+        ShowAllData();
+    }
+
+    public void ShowAllData() {
+        Logger.d("Show Method");
+        //progressDialog.show();
+
+        ApiInterface apiInterface = RetrofitApiClient.getRetrofit().create(ApiInterface.class);
+
+        Call<ServerResponse> call = apiInterface.getApiData();
+        call.enqueue(new Callback<ServerResponse>() {
+            @Override
+            public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
+                // progressDialog.dismiss();
+
+                ServerResponse serverResponse = response.body();
+                if (response.code() == 200 && serverResponse != null) { //response code 200 means server call successful
+                    //data found. So place the data into TextView
+
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ServerResponse> call, Throwable t) {
+                Logger.d(t.getMessage());
+            }
+        });
     }
 }
--- app/src/main/java/com/codercamp/showipusingmvp/network/ApiInterface.java
+++ app/src/main/java/com/codercamp/showipusingmvp/network/ApiInterface.java
@@ -1,4 +1,12 @@
 package com.codercamp.showipusingmvp.network;
 
+import com.codercamp.showipusingmvp.ServerResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ApiInterface {
+
+    @GET("/json")
+    Call<ServerResponse> getApiData();
 }
